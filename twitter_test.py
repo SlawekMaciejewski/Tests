38b15a4b26ab41fbb6277bbeac75118a9d2dac93
SlawekMaciejewski <slawek74.m@interia.pl>
2020-01-21 20:36:52 +0100

    Added method setUp(self) and refactored other tests for added method

--- twitter_test.py
+++ twitter_test.py
@@ -4,19 +4,18 @@ from twitter import Twitter
 
 
 class TwitterTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.twitter = Twitter()
+
     def test_initialization(self):
-        # Given
-        twitter = Twitter()
         # Then
-        self.assertTrue(twitter)
+        self.assertTrue(self.twitter)
 
     def test_tweet_single(self):
-        # Given
-        twitter = Twitter()
         # When
-        twitter.tweet('Test message')
+        self.twitter.tweet('Test message')
         # then
-        self.assertEqual(twitter.tweets, ['Test message'])
+        self.assertEqual(self.twitter.tweets, ['Test message'])
 
 
 if __name__ == '__main__':
