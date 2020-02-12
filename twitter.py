ea823c69c0aa10b2b4292b3c219285cbee227c1b
SlawekMaciejewski <slawek74.m@interia.pl>
2020-02-12 20:13:07 +0100

    Added parametres for @pytest.fixtures. In file, tweeter.py added features with a possibility to write to file .txt besides to write to list.

--- twitter.py
+++ twitter.py
@@ -1,26 +1,44 @@
+import os
 import re
 
 
 class Twitter(object):
     version = '1.0'
 
-    def __init__(self):
-        self.tweets = []
+    def __init__(self, backend=None):
+        self.backend = backend
+        self._tweets = []
+        if self.backend and not os.path.exists(self.backend):
+            with open(self.backend, 'w'):
+                pass
+
+    @property
+    def tweets(self):
+        if self.backend and not self._tweets:
+            with open(self.backend) as twitter_file:
+                self._tweets = [line.strip('\n') for line in twitter_file]
+        return self._tweets
 
     def delete(self):
-        print("It's the end")
+        if self.backend:
+            # with open('test.txt', 'w') as file_test:
+            #     file_test.write('')
+            os.remove(self.backend)
 
     def tweet(self, message):
         if len(message) > 160:
             raise Exception('Massage too long.')
         self.tweets.append(message)
+        if self.backend:
+            with open(self.backend, 'w') as twitter_file:
+                twitter_file.write('\n'.join(self.tweets))
 
     def find_hashtags(self, massage):
-        return [m.lower() for m in re.findall('#(\w+)', massage)]
+        return [m.lower() for m in re.findall(r'#(\w+)', massage)]
 
 
 if __name__ == '__main__':
-    twitter = Twitter()
+    twitter = Twitter(backend='tweets.txt')
     print(twitter.version, twitter.tweets)
     twitter.tweet('This is a test message')
     twitter.tweet('This is a test message2')
